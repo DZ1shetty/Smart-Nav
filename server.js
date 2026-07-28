ed5578edc0d44f926ea3821d69bb811a12d092dc
Dhanush G Shetty <dhanushgshetty666@gmail.com>
2026-07-28 20:42:57 +0530

    fix(server): update wildcard path for Express 5 compatibility

--- server.js
+++ server.js
@@ -420,7 +420,7 @@ app.post('/api/activity-log', async (req, res) => {
 const distPath = path.join(__dirname, 'dist');
 if (fs.existsSync(distPath)) {
   app.use(express.static(distPath));
-  app.get('*', (req, res, next) => {
+  app.use((req, res, next) => {
     if (req.path.startsWith('/api/')) return next();
     res.sendFile(path.join(distPath, 'index.html'));
   });
